fd9750afbc1258134d21d739252fb75bcf1e898e
umair-jami <lalaumair92@gmail.com>
2026-04-21 15:59:24 +0500

    added slider project

---
+++ touch-slider/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Slider from './components/Slider'
+
+const App = () => {
+  return (
+    <>
+    <Slider/>
+    </>
+  )
+}
+
+export default App
